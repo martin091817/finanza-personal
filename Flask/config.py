7ee08e1bc734584b951bac579e8ce5a36482baee
martin091817 <masa091817@gmail.com>
2024-11-07 16:48:59 -0500

    Variables de entorno, .gitignore, y db

--- Flask/config.py
+++ Flask/config.py
@@ -1,19 +1,23 @@
 import os
-from datetime import timedelta  # Importar timedelta para configurar la expiración del token
+from datetime import timedelta
+from dotenv import load_dotenv
+
+# Cargar las variables de entorno desde .env
+load_dotenv()
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     # Clave secreta para la aplicación Flask
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'aP8s09@#lM!(2e&KZpQdVb2R%uGf'
+    SECRET_KEY = os.getenv('SECRET_KEY', 'clave_por_defecto')
     
     # Clave secreta para firmar los tokens JWT
-    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or '2e2d2a4fbb914a49b6ae13d3457c31d45ae7b1c9a2c6a9d0516bb23248d3ec60'
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'clave_jwt_por_defecto')
     
-    # Configuración de la base de datos SQLite
-    SQLALCHEMY_DATABASE_URI = f'sqlite:///{os.path.join(BASE_DIR, "app.db")}'
+    # Configuración de la base de datos
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI', f'sqlite:///{os.path.join(BASE_DIR, "app.db")}')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    # Configuración para que el token expire en 1 hora
-    JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)  # El token expira en 1 hora
+    # Configuración de expiración del token JWT
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=int(os.getenv('JWT_EXPIRATION_HOURS', 1)))
 
