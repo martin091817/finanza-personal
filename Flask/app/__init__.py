3e6c270aa0aed38353cd54131f31f53ecf5cd2cb
martin091817 <masa091817@gmail.com>
2024-11-07 19:21:19 -0500

    Correccion error

--- Flask/app/__init__.py
+++ Flask/app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager  # Nuevo: Importar JWTManager
-from config import Config
+from Flask.config import Config
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
